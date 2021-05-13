5f259f4f2210fc7dd7af1787cdf2b3832c0fea44
Chong Lei <clei@sold.com>
2021-05-12 23:05:13 -0700

    installed fontawesome and styled main section

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithubSquare, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faAngleDoubleDown  } from '@fortawesome/free-solid-svg-icons'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="app">
+      <div className='main-section'>
+        <div>
+          <h1>Chong Lei</h1>
+          <h3>
+            Front-end Software Engineer
+            <br />
+            Irvine, CA
+          </h3>
+
+          <a href='https://www.linkedin.com/in/chongklei/' target='_blank'>
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href='https://github.com/chonglei24' target='_blank'>
+            <FontAwesomeIcon icon={faGithubSquare} />
+          </a>
+
+          <br />
+          <FontAwesomeIcon className='arrow-down' icon={faAngleDoubleDown} />
+        </div>
+      </div>
     </div>
   );
 }
