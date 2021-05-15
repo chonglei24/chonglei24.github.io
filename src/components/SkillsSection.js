f5e4aaa0c1e9ba384835a7d17c52095e60e5b7fc
Chong Lei <clei@sold.com>
2021-05-14 21:21:50 -0700

    added labels to skill icons

--- src/components/SkillsSection.js
+++ src/components/SkillsSection.js
@@ -9,17 +9,17 @@ import reduxLogo from '../assets/redux-logo.png';
 import '../App.scss';
 
 const LEFT_COLUMN_ICONS = [
-  <FontAwesomeIcon icon={faReact} />,
-  <FontAwesomeIcon icon={faJsSquare} />,
-  <img alt='next.js logo' src={nextJsLogo} />,
-  <img alt='redux logo' src={reduxLogo} style={{ width: '45px' }} />
+  { icon: <FontAwesomeIcon icon={faReact} />, label: 'React' },
+  { icon: <FontAwesomeIcon icon={faJsSquare} />, label: 'JavaScript'},
+  { icon: <img alt='next.js logo' src={nextJsLogo} />, label: 'Next.js' },
+  { icon: <img alt='redux logo' src={reduxLogo} style={{ width: '45px' }} />, label: 'Redux'}
 ]
 
 const RIGHT_COLUMN_ICONS = [
-  <FontAwesomeIcon icon={faHtml5} />,
-  <FontAwesomeIcon icon={faCss3Alt} />,
-  <FontAwesomeIcon icon={faGit} />,
-  <img alt='cypress.io logo' src={cypressLogo} />,
+  { icon: <FontAwesomeIcon icon={faHtml5} />, label: 'HTML'},
+  { icon: <FontAwesomeIcon icon={faCss3Alt} />, label: 'CSS'},
+  { icon: <FontAwesomeIcon icon={faGit} />, label: 'Git'},
+  { icon: <img alt='cypress.io logo' src={cypressLogo} />, label: 'Cypress.io'},
 ]
 
 const SkillsSection = React.forwardRef((props, ref) => {
@@ -34,11 +34,12 @@ const SkillsSection = React.forwardRef((props, ref) => {
           <Col xs={6}>
             <Container>
               <Row>
-                {LEFT_COLUMN_ICONS.map((icon, i) => (
+                {LEFT_COLUMN_ICONS.map(({ icon, label }, i) => (
                   <Col xs={12} lg={6} className='icon-col' key={i}>
                     <div className='white-border'>
                       {icon}
                     </div>
+                    <p>{label}</p>
                   </Col>
                 ))}
               </Row>
@@ -48,11 +49,12 @@ const SkillsSection = React.forwardRef((props, ref) => {
           <Col xs={6}>
             <Container>
               <Row>
-              {RIGHT_COLUMN_ICONS.map((icon, i) => (
+              {RIGHT_COLUMN_ICONS.map(({ icon, label }, i) => (
                   <Col xs={12} lg={6} className='icon-col' key={i}>
                     <div className='white-border'>
                       {icon}
                     </div>
+                    <p>{label}</p>
                   </Col>
                 ))}
               </Row>
